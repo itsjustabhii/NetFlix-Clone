d9b5189a314031f6c33fb9185febd31f32d92095
itsjustabhii <itsjustabhii@gmail.com>
2023-01-31 16:23:48 +0530

    added useState showPassword

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
 import BackgroundImage from '../components/BackgroundImage'
 import Header from '../components/Header'
 
 export default function Signup() {
+    const [showPassword, setShowPassword] = useState(false)
   return (
-    <Container>
+    <Container showPassword={showPassword}>
         <BackgroundImage/>
         <div className="content">        
         <Header login/>
@@ -17,8 +18,16 @@ export default function Signup() {
             </div>
             <div className="form">
                 <input type="email" placeholder='Email Address' name="email"  />
-                <input type="password" placeholder='Password' name='password' />
-                <button>Get Started</button>
+                {
+                    showPassword && (
+                        <input type="password" placeholder='Password' name='password' />
+                    )
+                }
+                
+                {
+                    !showPassword && (<button onClick={()=> setShowPassword(true)}>Get Started</button>)
+                }
+                
             </div>
             <button>Log In</button>
         </div>
@@ -38,5 +47,50 @@ position: relative;
     width: 100vw
     display: grid
     grid-template-rows: 15vh 85vh
+    .body {
+        gap: 1rem;
+        .text{
+            gap: 1rem;
+            text-align: center;
+            font-size: 2rem;
+            h1 {
+                padding: 0 25rem;
+            }
+        }
+        .form {
+            display: grid;
+            // grid-template-columns: ;
+            width: 60%;
+            input {
+                color:black;
+                border: none
+                padding: 1.5rem;
+                font-size: 1.2rem;
+                border: 1px solid black;
+                &:focus {
+                    outline: none;
+                }
+            }
+            button{
+                padding: 0.5rem 1rem
+                background-color: #e50914
+                border: none
+                cursor: pointer
+                color: white;
+                font-weight: bolder;
+                font-size: 1.05rem;
+            }
+        }
+        button {
+            padding: 0.5rem 1rem
+            background-color: #e50914
+            border: none
+            cursor: pointer
+            color: white;
+            border-radius: 0.2rem;
+            font-weight: bolder;
+            font-size: 1.05rem;
+        }
+    }
 }
 `
