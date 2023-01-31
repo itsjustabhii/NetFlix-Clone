a7721f82c2420cc050b8f7232fa5bf4fbf449707
itsjustabhii <itsjustabhii@gmail.com>
2023-01-31 14:47:31 +0530

    added signup component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Signup from './pages/Signup'
 
 export default function App() {
   return (
+    // <BrowserRouter>
+    // <Routes>
+    // <Route exact path ='/signup' element={<Signup/>} />
+    // </Routes>
+    // </BrowserRouter>
     <div>App</div>
   )
 }
