445f6cfff3123baa815504f583727955d53bc096
itsjustabhii <itsjustabhii@gmail.com>
2023-02-01 12:00:01 +0530

    added getGenres using axios

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,17 @@
 import {configureStore, createAsyncThunk, createSlice} from "@reduxjs/toolkit"
+import axios from 'axios';
+import {TMDB_BASE_URL, API_KEY} from "./utils/constants"
 
 const initialState = {
     movies: [],
     genresLoaded: false,
     genres: [],
 }
+
+export const getGenres = createAsyncThunk("netflix/genres", async()=> {
+    const {data} = await axios.get((`${TMDB_BASE_URL}/genre/movie/list?api_key${API_KEY}`))
+    console.log(data)
+})
 
 const NetflixSlice = createSlice({
     name: "Netflix",
