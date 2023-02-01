be632f8932d0b2fe9325f9534d4e69466f4975ca
itsjustabhii <itsjustabhii@gmail.com>
2023-02-01 13:23:39 +0530

    added getRawData

--- src/store/index.js
+++ src/store/index.js
@@ -9,10 +9,29 @@ const initialState = {
 }
 
 export const getGenres = createAsyncThunk("netflix/genres", async()=> {
-    const {data} = await axios.get((`${TMDB_BASE_URL}/genre/movie/list?api_key=${API_KEY}`))
-    console.log(data)
-    // return data
+    const {data:{genres}} = await axios.get((`${TMDB_BASE_URL}/genre/movie/list?api_key=${API_KEY}`))
+    return genres
 })
+
+const createArrayFromRawData = (array, moviesArray, genres) => {
+
+}
+
+const getRawData = async (api, genres, paging) => {
+    const moviesArray = []
+    for(let i = 1; moviesArray.length<60 && i< 10; i++) {
+        const {data:results} = await axios.get(`${api}${paging ? `&page=${i}`: ""}`)
+        createArrayFromRawData(results, moviesArray,genres)
+        return moviesArray
+    }
+}
+
+//To get Movies
+export const fetchMovies = createAsyncThunk("netflix/trending", async({type}, thunkApi) => {
+    const {netflix:{genres}} = thunkApi.getState()
+    return getRawData(`${TMDB_BASE_URL}/trending/${type}/week? api_key=${API_KEY}`, genres, true)
+})
+// return getRawData(`${TMDB_BASE_URL}/discover/${type}?api_key=${API_KEY} &with_genres=${genres}`)
 
 const NetflixSlice = createSlice({
     name: "Netflix",
