ed6247a44484b1ee49aa5bd50290ede10a02e700
itsjustabhii <itsjustabhii@gmail.com>
2023-02-01 16:21:35 +0530

    removed const data for getRawData

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import {configureStore, createAsyncThunk, createSlice} from "@reduxjs/toolkit"
 import axios from 'axios';
-import {TMDB_BASE_URL, API_KEY} from "./utils/constants"
+import {TMDB_BASE_URL, API_KEY} from "../utils/constants"
 
 const initialState = {
     movies: [],
@@ -14,23 +14,37 @@ export const getGenres = createAsyncThunk("netflix/genres", async()=> {
 })
 
 const createArrayFromRawData = (array, moviesArray, genres) => {
-
+    array.forEach((movie) => {
+        const movieGenres = []
+        movie.genre_ids.forEach((genre) => {
+            const name = genres.find(({id}) => id ===genre)
+            if(name) movieGenres.push(name.name)
+        })
+        if(movie.backdrop_path) 
+            moviesArray.push({
+                id:movie.id,
+                name:movie?.original_name ? movie.original_name : movie.original_title,
+                image: movie.backdrop_path,
+                genres: movieGenres.slice(0, 3)
+            })
+    })
 }
 
-const getRawData = async (api, genres, paging) => {
+const getRawData = async (api, genres, paging=false) => {
     const moviesArray = []
-    for(let i = 1; moviesArray.length<60 && i< 10; i++) {
-        const {data:results} = await axios.get(`${api}${paging ? `&page=${i}`: ""}`)
+    for(let i = 1; moviesArray.length < 60 && i < 10; i++) {
+        const {data:{results},} = await axios.get(`${api}${paging ? `&page=${i}`: ""}`)
         createArrayFromRawData(results, moviesArray,genres)
-        return moviesArray
+        return moviesArray   
     }
 }
 
 //To get Movies
 export const fetchMovies = createAsyncThunk("netflix/trending", async({type}, thunkApi) => {
-    const {netflix:{genres}} = thunkApi.getState()
-    return getRawData(`${TMDB_BASE_URL}/trending/${type}/week? api_key=${API_KEY}`, genres, true)
-})
+    const {netflix:{genres},} = thunkApi.getState()
+    getRawData(`${TMDB_BASE_URL}/trending/${type}/week? api_key=${API_KEY}`, genres, true)
+}
+)
 // return getRawData(`${TMDB_BASE_URL}/discover/${type}?api_key=${API_KEY} &with_genres=${genres}`)
 
 const NetflixSlice = createSlice({
@@ -40,6 +54,9 @@ const NetflixSlice = createSlice({
         builder.addCase(getGenres.fulfilled,(state,action) => {
             state.genres = action.payload
             state.genresLoaded = true
+        })
+        builder.addCase(fetchMovies.fulfilled,(state,action) => {
+            state.movies = action.payload
         })
     }
 })
