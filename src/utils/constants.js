873987836e8133969b7f32f406a5260bb64f04be
itsjustabhii <itsjustabhii@gmail.com>
2023-02-02 16:25:54 +0530

    edited API_KEY

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,4 @@
-export const API_KEY = "00b5d43aaa5d63c3c423669d7286c3e9"
+export const API_KEY = "3d39d6bfe362592e6aa293f01fbcf9b9"
 export const TMDB_BASE_URL = "https://api.themoviedb.org/3"
+
+//my API_KEY = "00b5d43aaa5d63c3c423669d7286c3e9"
