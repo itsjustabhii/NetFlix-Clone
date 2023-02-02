56b533899e5dbf79431088888fe01743d23f370c
itsjustabhii <itsjustabhii@gmail.com>
2023-02-02 16:27:15 +0530

    edited

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -2,8 +2,15 @@ import React, {useEffect, useState} from 'react'
 import {useDispatch, useSelector} from "react-redux"
 import {useNavigate} from "react-router-dom"
 import {fetchMovies, getGenres} from "../store"
+import { onAuthStateChanged} from "firebase/auth"
+import {firebaseAuth} from "../utils/firebase-config"
+import NotAvailable from "../components/NotAvailable"
+import Navbar from "../components/Navbar"
+import Slider from "../components/Slider"
+import styled from "styled-components";
+import SelectGenre from "../components/SelectGenre"
 
-export default function Movies() {
+function MoviePage() {
     const [isScrolled, setIsScrolled] = useState(false);
   const movies = useSelector((state) => state.netflix.movies);
   const genres = useSelector((state) => state.netflix.genres);
@@ -17,16 +24,44 @@ export default function Movies() {
   }, []);
 
   useEffect(() => {
-    if (genresLoaded) {
+    if (genresLoaded) 
       dispatch(fetchMovies({ genres, type: "movies" }));
-    }
   }, [genresLoaded]);
 
   window.onscroll = () => {
     setIsScrolled(window.pageYOffset === 0 ? false : true);
     return () => (window.onscroll = null);
   };
+
+  onAuthStateChanged(firebaseAuth,(currentUser) => {
+    // if(currentUser) navigate("/")
+})
+
     return (
-        <div>Movies</div>
+        <Container>
+            <div className="navbar">
+                <Navbar isScrolled={isScrolled}/>
+            </div>
+            <div className="data">
+            <SelectGenre genres={genres} type="movies" />            
+                {
+                    movies.length ? <Slider movies={movies}/> :
+                    <NotAvailable/>
+                }
+            </div>
+        </Container>
     )
 }
+
+const Container = styled.div`
+.data {
+    margin-top: 8rem;
+    .not-available {
+      text-align: center;
+      color: white;
+      margin-top: 4rem;
+    }
+  }
+`
+
+export default MoviePage;
