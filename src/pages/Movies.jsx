737110f018e29ca92e58c1bac95f4b6f2674ff1d
itsjustabhii <itsjustabhii@gmail.com>
2023-02-02 13:45:10 +0530

    added Movies file inside pages

---
+++ src/pages/Movies.jsx
@@ -0,0 +1,32 @@
+import React, {useEffect, useState} from 'react'
+import {useDispatch, useSelector} from "react-redux"
+import {useNavigate} from "react-router-dom"
+import {fetchMovies, getGenres} from "../store"
+
+export default function Movies() {
+    const [isScrolled, setIsScrolled] = useState(false);
+  const movies = useSelector((state) => state.netflix.movies);
+  const genres = useSelector((state) => state.netflix.genres);
+  const genresLoaded = useSelector((state) => state.netflix.genresLoaded);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getGenres());
+  }, []);
+
+  useEffect(() => {
+    if (genresLoaded) {
+      dispatch(fetchMovies({ genres, type: "movies" }));
+    }
+  }, [genresLoaded]);
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+    return (
+        <div>Movies</div>
+    )
+}
