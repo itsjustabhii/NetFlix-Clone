4607584fed1418939ecb48b035532008608117dd
itsjustabhii <itsjustabhii@gmail.com>
2023-02-02 16:21:40 +0530

    added TVShows Route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./pages/Login"
 import Netflix from "./pages/Netflix"
 import Player from "./pages/Player"
 import Movies from "./pages/Movies"
+import TVShows from "./pages/TVShows"
 
 export default function App() {
   return (
@@ -13,7 +14,8 @@ export default function App() {
         <Route exact path ='/login' element={<Login/>} />
         <Route exact path ='/signup' element={<Signup/>} />
         <Route exact path="player" element={<Player/>}/ >
-          <Route exact path="/movies" element={<Movies/>} />
+        <Route exact path="/movies" element={<Movies/>} />
+        <Route exact path="/tv" element={<TVShows/>}/>
         <Route exact path ='/' element={<Netflix/>} />
     </Routes>
     </BrowserRouter>
