ea2f9500f7ebb00ceb2836e0a6834fde4945c13a
itsjustabhii <itsjustabhii@gmail.com>
2023-02-08 17:56:41 +0530

    added userRoutes for getLikedMovies

--- netflix-api/routes/UserRoutes.js
+++ netflix-api/routes/UserRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router()
-const {addToLikedMovies} = require("../controllers/UserController")
+const {addToLikedMovies, getLikedMovies} = require("../controllers/UserController")
 
 router.post("/add", addToLikedMovies)
+router.arguments("/liked/:email", getLikedMovies)
 
 module.exports = router
