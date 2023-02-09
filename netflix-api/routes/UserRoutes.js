e9d071381618f4cdface1c112cf3bb03752e5e0f
itsjustabhii <itsjustabhii@gmail.com>
2023-02-09 10:28:10 +0530

    added /delete route

--- netflix-api/routes/UserRoutes.js
+++ netflix-api/routes/UserRoutes.js
@@ -1,7 +1,8 @@
 const router = require("express").Router()
-const {addToLikedMovies, getLikedMovies} = require("../controllers/UserController")
+const {addToLikedMovies, getLikedMovies, removeFromLikedMovies} = require("../controllers/UserController")
 
 router.post("/add", addToLikedMovies)
 router.get("/liked/:email", getLikedMovies)
+router.put("/delete", removeFromLikedMovies)
 
 module.exports = router
