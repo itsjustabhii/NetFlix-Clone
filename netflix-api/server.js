5d39d91e6cf6863afc16e6a7289ee7d3e1e4a252
itsjustabhii <itsjustabhii@gmail.com>
2023-02-09 10:24:24 +0530

    added removeFromLikedMovies

--- netflix-api/server.js
+++ netflix-api/server.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const cors = require("cors")
+const userRoutes = require("./routes/UserRoutes")
 const mongoose = require("mongoose")
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+mongoose.connect("mongodb+srv://abhishek:abhishek@cluster0.tfwdnut.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser:true,
+    useUnifiedTopology:true}).then(() => {
+        console.log("DB Connected!")
+    })
+
+    app.use("/api/user", userRoutes)
 
 app.listen(5000, console.log("Server started!"))
